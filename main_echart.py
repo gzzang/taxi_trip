16187c25bba9a7d8174e9bfe15ff802180142055
gzzang <zangguangzhi@qq.com>
2020-03-30 21:47:52 +0800

    修改为调用数据预处理函数

--- main_echart.py
+++ main_echart.py
@@ -13,72 +13,7 @@ from pyecharts.charts import Geo
 from pyecharts import options as opts
 from pyecharts.globals import ChartType
 
-
-def drop_noisy(df, columns_str):
-    df_copy = df.copy()
-    df_describe = df_copy.describe()
-    for column in columns_str:
-        mean = df_describe.loc['mean', column]
-        std = df_describe.loc['std', column]
-        minvalue = mean - 4 * std
-        maxvalue = mean + 4 * std
-        df_copy = df_copy[df_copy[column] >= minvalue]
-        df_copy = df_copy[df_copy[column] <= maxvalue]
-    return df_copy
-
-
-def get_airport(df, column_str):
-    airport_min_longitude = 116.573599
-    airport_max_longitude = 116.626814
-    airport_min_latitude = 40.047361
-    airport_max_latitude = 40.108833
-
-    df_copy = df.copy()
-    df_copy = df_copy[df_copy[column_str + '_longitude'] >= airport_min_longitude]
-    df_copy = df_copy[df_copy[column_str + '_longitude'] <= airport_max_longitude]
-    df_copy = df_copy[df_copy[column_str + '_latitude'] >= airport_min_latitude]
-    df_copy = df_copy[df_copy[column_str + '_latitude'] <= airport_max_latitude]
-    return df_copy
-
-
-def cal_coordinate_count_of_one_day(file_path, is_airport_initiation, is_output_airport=False):
-    coordinate_list = ['init', 'term']
-    airport_coordinate_type = coordinate_list[not is_airport_initiation]
-    another_coordinate_type = coordinate_list[is_airport_initiation]
-
-    full_df = pd.read_csv(file_path, header=None)
-    full_df.columns = ['id', 'date', 'hour', 'init', 'term', 'a', 'b']
-
-    init_value = full_df['init'].to_numpy()
-    init_longitude = np.floor_divide(init_value, 100000) / 1000
-    init_latitude = np.mod(init_value, 100000) / 1000
-
-    full_df['init_longitude'] = init_longitude
-    full_df['init_latitude'] = init_latitude
-
-    term_value = full_df['term'].to_numpy()
-    term_longitude = np.floor_divide(term_value, 100000) / 1000
-    term_latitude = np.mod(term_value, 100000) / 1000
-
-    full_df['term_longitude'] = term_longitude
-    full_df['term_latitude'] = term_latitude
-
-    nonzero_df = full_df[(full_df['term'] != 0) & (full_df['init'] != 0)]
-    standard_df = drop_noisy(nonzero_df, ['init_latitude', 'init_longitude', 'term_latitude', 'term_longitude'])
-
-    airport_init_df = get_airport(standard_df, airport_coordinate_type)
-    if is_output_airport:
-        count_point_df = airport_init_df.groupby(
-            [airport_coordinate_type + "_longitude", airport_coordinate_type + "_latitude"]).size().reset_index(
-            name="time")
-    else:
-        count_point_df = airport_init_df.groupby(
-            [another_coordinate_type + "_longitude", another_coordinate_type + "_latitude"]).size().reset_index(
-            name="time")
-    count_point_df.columns = ['longitude', 'latitude', 'time']
-
-    return count_point_df
-
+from preparation import cal_coordinate_count_of_one_day
 
 def plot(is_car_taxi, is_airport_initiation, is_output_airport=False):
     car_type = 'taxi' if is_car_taxi else 'online'
